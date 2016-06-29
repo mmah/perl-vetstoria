943cb9b73fb53e284734590c8f558f68dac23467
William Page <wpage@vetstreet.com>
2016-06-29 09:54:28 -0400

    Initial commit.  This version was tested with Vetstoria and is working.

---
+++ testing/Vetstoria_Test.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Vetstoria;
+
+my $vetstoria = Vetstoria->new();
+
+my $url = $vetstoria->CreateURL("HHHHHHH", "CCCCCCCC", "PPPPPPP");
+
+print "\n$url\n\n";
+
+my($hosp, $client, $patient) = $vetstoria->DecodeURL($url);
+
+print "Hospital = '$hosp'\nClient='$client'\nPatient='$patient'\n";
